b83452e61aa907d40075ec9788e75929db3d9b86
Khai <phamphuckhai@gmail.com>
2020-04-17 15:18:25 +0700

    connect to mysql

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
     res.json({message: 'Express is up!'});
@@ -12,3 +12,18 @@ app.get('/', function(req, res){
 app.listen(3000, function(){
     console.log('Express is running on port 3000');
 });
+
+//init sequeliz
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+    database: 'users_db',
+    username: 'root',
+    password: '',
+    dialect: 'mysql',
+});
+
+//check connect
+sequelize
+    .authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(err => console.error('Unable to connect to the database', err));
