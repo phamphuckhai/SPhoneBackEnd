a09207cd7ae510286831934e065110aff7d00429
Khai <phamphuckhai@gmail.com>
2020-04-18 17:43:27 +0700

    authentication system

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const jwt = require('jsonwebtoken');
+
+//import passport and passport-jwt modules
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+//Extract token
+let ExtractJwt = passportJWT.ExtractJwt;
+
+//strategy for authentication
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = "imhandsome";
+
+//strategy for web token
+let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next){
+    console.log('payload received', jwt_payload);
+    let user = getUser({id: jwt_payload.id});
+    if (user){
+        next(null, user);
+    }
+    else{
+        next(null, flase)
+    }
+});
+//use the strategy
+passport.use(strategy);
+app.use(passport.initialize());
 
 //connect sequelize
 const {sequelize} = require('./models');
@@ -28,7 +57,35 @@ app.post('/register', function(req, res, next){
     createUser({name, password}).then(users => 
         res.json({users, msg: 'account created successfully '})
         );
-})
+});
+//login route
+app.post('/login', async function(req, res, next) {
+    const {name, password} = req.body;
+    if(name && password){
+        let user = await getUser({name});
+        if(!user){
+            res.status(401).json({msg: 'No such user found', user});
+        }
+
+        if(user.password===password)
+        {
+            let payload = {id: user.id};
+            let token = jwt.sign(payload, jwtOptions.secretOrKey);
+            res.json({msg: 'Ok', token: token});
+        }
+        else{
+            res.status(401).json({msg: 'Password is incorrect', user});
+        }
+    }
+});
+
+//protected route
+
+app.get('/protected', passport.authenticate('jwt', {session: false}), function(req, res){
+    res.json({
+        msg: 'Congrats! You are seeing this because you are authorized'
+    });
+});
 
 //check connect
 sequelize
