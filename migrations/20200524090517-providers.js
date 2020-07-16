34d266a7c112b4078c0955cde96f8e0dd8bb0dba
Khai Pham <phamphuckhai@gmail.com>
2020-07-17 00:09:30 +0700

    fix something

--- migrations/20200524090517-providers.js
+++ migrations/20200524090517-providers.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       phone: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
       },
       website: {
         type: Sequelize.STRING,
